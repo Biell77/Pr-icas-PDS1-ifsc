dab41d80527df87b0567de96ab9143e0aa35fbf2
Gabriel <gabriel.r17@aluno.ifsc.edu.br>
2023-02-28 08:59:44 -0300

    Lista1
    exercicio1

---
+++ Lista1/src/Exercicio1/ex1.java
@@ -0,0 +1,16 @@
+package Exercicio1;
+
+import javax.swing.JOptionPane;
+
+public class ex1 {
+	
+	public static void main(String[] args) {
+	
+	Double precoLitro = 0;
+	Integer quantidadeLitros = null;
+	
+	
+	
+	
+	}
+}
---
+++ Lista1/src/Visao/Interface.java
@@ -0,0 +1,44 @@
+package Visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+
+public class Interface extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Interface frame = new Interface();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Interface() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+	
+	
+
+}
