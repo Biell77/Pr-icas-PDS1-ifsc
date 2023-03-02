a6f408623a005b3e478be937e54f29100eea1541
Biell <gabriel.r17@aluno.ifsc.edu.br>
2023-03-02 01:04:33 -0300

    Lista1

--- Lista1/src/Exercicio1/ex1.java
+++ Lista1/src/Exercicio1/ex1.java
@@ -6,11 +6,20 @@ public class ex1 {
 	
 	public static void main(String[] args) {
 	
-	Double precoLitro = 0;
-	Integer quantidadeLitros = null;
+	double PrecoLitro = 0;
+	Integer QuantidadeLitros = null;
 	
+	PrecoLitro = Double.valueOf(JOptionPane.showInputDialog("Insira qual será o valor da gasolina"));
+	QuantidadeLitros = Integer.valueOf(JOptionPane.showInputDialog("Insira a quantidade de gasolina"));
 	
+	JOptionPane.showMessageDialog(null, "O preço total é :" +CalculoTotal (QuantidadeLitros, PrecoLitro) );
 	
+	}
+	
+	public static Double CalculoTotal (Integer QuantidadeLitros, Double PrecoLitro) {
+		
+		Double TotalPagamento = QuantidadeLitros * PrecoLitro;
+		return TotalPagamento;
 	
 	}
 }
--- Lista1/src/Visao/Interface.java
+++ Lista1/src/Visao/Interface.java
@@ -5,10 +5,10 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
-import javax.swing.JButton;
 
 public class Interface extends JFrame {
 
+	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 
 	/**
