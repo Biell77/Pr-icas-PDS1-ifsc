20659f8cadf125369e6bf2ef869cc2f61fb87cb3
Gabriel <gabriel.r17@aluno.ifsc.edu.br>
2023-03-14 08:49:19 -0300

    lista1

---
+++ Lista1/src/Exercicio3/ex3.java
@@ -0,0 +1,35 @@
+package Exercicio3;
+
+import javax.swing.JOptionPane;
+
+public class ex3 {
+	
+	public static void main(String[] args) {
+		
+		Double Nota1;
+		Double Nota2;
+		Double Nota3;
+		
+		Nota1 = Double.valueOf(JOptionPane.showInputDialog("Insira a primeira nota: "));
+		Nota2 = Double.valueOf(JOptionPane.showInputDialog("Insira a segunda nota: "));
+		Nota3 = Double.valueOf(JOptionPane.showInputDialog("Insira a terceira nota: "));
+		
+		JOptionPane.showMessageDialog(null, "Media Final: " +mediaf(Nota1, Nota2, Nota3));
+		
+			
+		}
+
+	private static Double mediaf(double Nota1, double Nota2, double Nota3) {
+		Double MediaFinal = (Nota1 + Nota2 + Nota3)/3;
+		
+		if(MediaFinal >= 6) {
+			JOptionPane.showMessageDialog(null, "Aprovado");
+		} if (MediaFinal >= 4) {
+			JOptionPane.showMessageDialog(null, "Recuperação");
+		} if(MediaFinal < 4) {
+			JOptionPane.showMessageDialog(null, "Reprovado");
+		}
+		return MediaFinal;
+	}
+
+}
---
+++ Lista1/src/Exercicio4/ex4.java
@@ -0,0 +1,10 @@
+package Exercicio4;
+
+public class ex4 {
+	
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
